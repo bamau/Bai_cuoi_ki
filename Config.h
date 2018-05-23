e070aae47b7c6b43565b0cdbb11e073d51785545
Tran Ba Mau <bamau97@gmail.com>
2018-05-23 22:56:52 +0700

    Up load bai cuoi ky

---
+++ Config.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Image
+{
+	private:
+		int width;
+		int height;
+		int gray_levels;
+		char **pixels;
+	public:
+		
+};
+class Filter
+{
+	private:
+		int width;
+		int height;
+		char**Maxtrix;
+	public:
+		
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Config.h"
+using namespace std;
+
+int main()
+{
+	
+}
+
